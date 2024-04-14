07476e9fa25660d0c3f0e6c5ff277ca6daced8fd
samaimo <kii328815@gmail.com>
2024-04-14 09:12:59 +0900

    deviseをカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # どのコントローラーと紐づけるのかを明示する。今回は新規登録に必要なregistrationsコントローラーのみに適用
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: "home#index"
 end
